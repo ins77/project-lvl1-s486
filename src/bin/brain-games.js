5c42fdae211dae62002c907bef65fa97665dea86
d.sukhinin <d.sukhinin@tinkoff.ru>
2019-05-04 12:28:01 +0300

    Подключил eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import sayHello from "..";
+import sayHello from '..';
 
 sayHello();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!\n");
+console.log('Welcome to the Brain Games!\n');
 
-const name = readlineSync.question("May I have your name? ");
+const name = readlineSync.question('May I have your name? ');
 const sayHello = () => {
   console.log(`Hello, ${name}!`);
 };
