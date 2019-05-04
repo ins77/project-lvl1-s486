0fe8143bc25313ef502c58b0fe713776d67f1501
d.sukhinin <d.sukhinin@tinkoff.ru>
2019-05-04 14:41:50 +0300

    реализовал игру Проверка на четность

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { sayHello, startGame } from '..';
+
+sayHello();
+startGame();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!\n');
+const NUMBER_OF_QUESTIONS = 3;
+const ANSWER_CORRECT = 'Correct!';
 
-const name = readlineSync.question('May I have your name? ');
-const sayHello = () => {
-  console.log(`Hello, ${name}!`);
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+
+const name = readlineSync.question('\nMay I have your name? ');
+
+export const sayHello = () => {
+  console.log(`Hello, ${name}!\n`);
 };
 
-export default sayHello;
+const getAnswerResultText = (randomNumber, answer) => {
+  const isEven = randomNumber % 2 === 0;
+
+  if ((isEven && answer === 'yes') || (!isEven && answer === 'no')) {
+    return ANSWER_CORRECT;
+  }
+
+  return isEven && answer !== 'yes'
+    ? `"no" is wrong answer ;(. Correct answer was "yes". Let's try again, ${name}!`
+    : `"yes" is wrong answer ;(. Correct answer was "no". Let's try again, ${name}!`;
+};
+
+const startQuestions = (count, value, questionsNumber) => {
+  if (value !== ANSWER_CORRECT) {
+    return null;
+  }
+
+  if (count > questionsNumber) {
+    console.log(`Congratulations, ${name}!`);
+
+    return null;
+  }
+
+  const randomNumber = Math.ceil(Math.random() * 100);
+
+  console.log(`Question: ${randomNumber}`);
+
+  const answer = readlineSync.question('Your answer: ');
+  const answerResult = getAnswerResultText(randomNumber, answer);
+
+  console.log(answerResult);
+
+  return startQuestions(count + 1, answerResult, questionsNumber);
+};
+
+export const startGame = () => startQuestions(1, ANSWER_CORRECT, NUMBER_OF_QUESTIONS);
